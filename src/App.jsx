74844b65bc2bef2c28f53567fbb4d31a8ae96320
Omolade Kelvin <omoladekelvin05@gmail.com>
2025-01-31 13:55:30 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoListForm from "./components/TodoListForm";
+
+const App = () => {
+  return (
+    <div className="w-[35vw] pt-[4rem] rounded-2xl mx-auto h-[94vh] bg-[#0F303B] ">
+      <TodoListForm></TodoListForm>
+    </div>
+  );
+};
+
+export default App;
