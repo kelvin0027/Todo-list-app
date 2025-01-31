a356f5a5a8da831cd181f9a23b1fcd8550b323db
Omolade Kelvin <omoladekelvin05@gmail.com>
2025-01-31 23:01:28 +0100

    Display message when todos array is empty

--- src/components/TodoAppList.jsx
+++ src/components/TodoAppList.jsx
@@ -13,11 +13,13 @@ const TodoAppList = ({ todos, setTodos, isLoading, isDeleting, setIsDeleting }) 
     <div>
       <div
         className={`w-[85%] ${
-          isLoading || isDeleting ? `flex  justify-center pb-14 items-center` : ""
+          isLoading || isDeleting || todos.length === 0 ? `flex pl-5 justify-center pb-14 items-center` : ""
         } overflow-y-scroll pr-2 scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 space-y-3 pt-5 mx-auto h-[60vh]`}
       >
         {isLoading || isDeleting ? (
           <span className={`loading ${isDeleting ? "text-error" : ""} loading-spinner size-10 text-accent`}></span>
+        ) : todos.length === 0 ? (
+          <p className="text-center text-xl text-gray-300 italic">No tasks available. Add a new todo!</p>
         ) : (
           todos.map((todo) => (
             <TodoAppItem deleteBtnHandler={() => deleteBtnHandler(todo.id)} key={todo.id}>
